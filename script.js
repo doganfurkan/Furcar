067c41652c92e014ca95cdf029a0a30684d7ad84
Furkan Doğan <1furkandogan1@gmail.com>
2023-07-21 16:39:12 +0300

    Touch slider for desktop is added

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ function closeMenu(){
     document.querySelector("#entrance nav").classList.remove("active")
     document.querySelector("#menuClose").classList.remove("active")
 }
+
+
+// ------------------------- Touch Slider ---------------------
+var scrollPosition = 0,
+chosenSlider,
+startedScrolling = false;
+
+if(window.screen.width > 740){
+    Array.from(document.querySelectorAll(".touchSlider")).forEach((slider) => {
+        slider.addEventListener("mousedown",(e) => {
+            slider.classList.add("active")
+            startPosition = e.pageX;
+            chosenSlider = slider.id;
+            startScrolling()
+        });
+        slider.addEventListener("mouseup",(e) => {
+            slider.classList.remove("active")
+            document.getElementById(slider.id).removeEventListener("mousemove", startScroll)
+            console.log(e.pageX)
+            startPosition = e.pageX;
+            scrollPosition = document.getElementById(slider.id).scrollLeft;
+            startedScrolling = false;
+        })
+        slider.addEventListener("scroll",() => {
+            if(!startedScrolling){
+                scrollPosition = document.getElementById(slider.id).scrollLeft;
+            }
+        })
+    })
+}
+
+function startScrolling(){
+    document.querySelector(`#${chosenSlider}.active`).addEventListener("mousemove",startScroll)
+}
+
+function startScroll(e){
+    startedScrolling = true;
+    document.getElementById(chosenSlider).scrollTo(scrollPosition + startPosition - e.pageX,0)
+}
